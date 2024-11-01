8bea231a9a90c60f9f0b7fa64aa68ba3a0de023a
İbrahim Halil Akgül <154886268+ibrahimhalilakgl@users.noreply.github.com>
2024-11-01 17:19:56 +0300

    problem1.c

--- problem1.c
+++ problem1.c
@@ -3,27 +3,27 @@
 
 int main(){
 	
-	int sayi,i,toplam=0;
-	
-	printf("Lutfen Sayiyi Giriniz: ");
-	scanf("%d",&sayi);
-	
-	for(i=1;i<sayi;i++)
+	int sayi, i, toplam = 0; // 'sayi' kullanıcının girdiği sayı, 'i' döngü değişkeni, 'toplam' bölenlerin toplamını tutuyor
+
+	printf("Lutfen Sayiyi Giriniz: "); // Kullanıcıdan bir sayı girmesini istiyoruz
+	scanf("%d", &sayi); // Girilen sayıyı 'sayi' değişkenine atıyoruz
+
+	for(i = 1; i < sayi; i++) // 1'den girilen sayıya kadar döngü başlatıyoruz
 	{
-		if(sayi%i==0)
+		if(sayi % i == 0) // 'i' sayısının, 'sayi'ya tam bölünüp bölünmediğini kontrol ediyoruz
 		{
-			toplam+=i;
+			toplam += i; // Eğer tam bölünüyorsa, 'toplam' değişkenine ekliyoruz
 		}
 	}
 	
-	if(toplam==sayi)
+	if(toplam == sayi) // Eğer bölenlerin toplamı girilen sayıya eşitse
 	{
-		printf("Sayi Mukemmel Sayidir.");
+		printf("Sayi Mukemmel Sayidir."); // Sayının mükemmel sayı olduğunu yazdırıyoruz
 	}
-	else
+	else // Değilse
 	{
-		printf("Sayi Mukemmel Sayi Degildir.");
+		printf("Sayi Mukemmel Sayi Degildir."); // Sayının mükemmel sayı olmadığını yazdırıyoruz
 	}
 	
-	return 0;
+	return 0; // Programı başarıyla sonlandırıyoruz
 }
